39a9c35760bbf14969dcbe39a0986f303cb5f7a7
Dariush <d.bakhtvar@gmail.com>
2022-06-01 23:40:37 +0430

    Task list logic

--- src/Components/Tasks/Tasks.js
+++ src/Components/Tasks/Tasks.js
@@ -7,15 +7,17 @@ const Tasks = ({ Task }) => {
   return (
     <div className={styles.TaskWrapper}>
       <div className={styles.TaskIcons}>
-        <CircularProgress cx={50} cy={50} r={35} />
-        <DynamicIcon name={Task.icon} />
+        <CircularProgress cx={50} cy={50} r={35} isActive={true} />
+        <span className={styles.iconWrapper}>
+          <DynamicIcon name={Task.icon} />
+        </span>
       </div>
       <div className={styles.TaskContent}>
         <div className={styles.Task}>
           <p>{Task.WhatDo}</p>
           <p>{Task.WhereDo}</p>
         </div>
-        <div className={styles.TaskDate} onMouseOver={() => setHover(true)}>
+        <div className={styles.TaskDate} onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)}>
           {Task.TimeDo[0]}
           {hover && <div>
             <button>1</button>
